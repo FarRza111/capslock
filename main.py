647fc9c0f1dc381accf0a2d91af212d1459a7436
FarRza111 <farrza111@gmail.com>
2025-06-19 23:53:35 +0300

    task completed

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import pandas as pd
+def main():
+
+    my_text = ("Hello!"
+               "I will pass the Interview Stage ,"
+               "Best Regards, Fariz ")
+
+    print(my_text)
+
+
+if __name__ == '__main__':
+    main()
